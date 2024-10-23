ad72a8139a4b8f3d09ab6b3a8a274d662ba8f8b4
akashkhot12 <akashkho03@gmail.com>
2024-10-23 16:55:52 +0530

    connection error

--- ecom-node1/server/server.js
+++ ecom-node1/server/server.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+require('dotenv').config();
 
+//CONNECT MONGO DB
+const URI = process.env.MONGODB_URL;
 
-
+mongoose.connect(URI).then(() => {
+    console.log("MongoDB connected.");
+}).catch((err) => {
+    console.error("MongoDB connection error:", err);
+});
 
 app.listen(PORT, () => {
-    console.log("server running.");
-})
-
-//CONNECT MONGO DB 
-
-const URI = process.env.MONGODB_URL
+    console.log(`Server running on port ${PORT}`);
+});
