8fbf9baf0e7bca0a24007c77ca136ac253f7dd32
akashkhot12 <akashkho03@gmail.com>
2024-10-23 19:04:21 +0530

    working all miodules

--- ecom-node1/server/controllers/userCtr.js
+++ ecom-node1/server/controllers/userCtr.js
@@ -1,9 +1,10 @@
 const Users = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
 const userController = {
     register: async (req, res) => {
+        console.log('Secret Key:', process.env.ACCESS_TOKEN_SECRET_KEY);
+
         try {
             const { name, email, password } = req.body;
 
@@ -29,12 +30,55 @@ const userController = {
             // Save user to the database
             await newUser.save();
 
-            res.json({ msg: "Registration successful." });
+            // Generate JWT access and refresh tokens
+            const accessToken = createAccessToken({ id: newUser._id });
+            const refreshToken = createRefreshToken({ id: newUser._id });
+
+            // Set refresh token in a cookie
+            res.cookie('refreshtoken', refreshToken, {
+                httpOnly: true,
+                path: '/user/refresh_token',  // This path will be used to send the refresh token
+                maxAge: 7 * 24 * 60 * 60 * 1000  // 7 days
+            });
+
+            // Send the access token in the response
+            res.json({ accessToken });
 
         } catch (error) {
             return res.status(500).json({ msg: error.message });
         }
+    },
+
+    refreshToken: async (req, res) => {
+        try {
+            // Access refresh token from the cookies
+            const rf_token = req.cookies.refreshtoken;
+
+            if (!rf_token) {
+                return res.status(400).json({ msg: "Please Login or Register" });
+            }
+
+            // Verify the refresh token
+            jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET_KEY, (err, user) => {
+                if (err) return res.status(400).json({ msg: "Please Login or Register" });
+
+                const accessToken = createAccessToken({ id: user.id });
+                res.json({ accessToken });
+            });
+        } catch (err) {
+            return res.status(500).json({ msg: err.message });
+        }
     }
 };
+
+// Function to create JWT access token
+const createAccessToken = (payload) => {
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET_KEY, { expiresIn: '1d' });
+}
+
+// Function to create JWT refresh token
+const createRefreshToken = (payload) => {
+    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET_KEY, { expiresIn: '7d' });
+}
 
 module.exports = userController;
